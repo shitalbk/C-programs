e0afd4aaec4605a07b7754db2c308ae8ffd8d0e9
Shital B.K <shitalbk36@gmail.com>
2015-01-12 15:30:13 +0545

    solved question no. 4

---
+++ 3rd_sem/lab_1/src/question4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+		cout<<left<<setw(15)<<"Location"<<"Population"<<endl;
+    cout<<"==========================\n";
+    cout<<left<<setw(20)<<"Kirtipur"<<12345<<endl;
+    cout<<left<<setw(20)<<"Patan"<<32452<<endl;
+    cout<<left<<setw(19)<<"Kathmnadu"<<556342<<endl;
+    cout<<left<<setw(21)<<"Manag"<<3421<<endl;
+   return 0;
+	
+
+}
